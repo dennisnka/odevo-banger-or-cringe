c14f9c9a7332e5443e9b8cc423bc3eb745d39c2f
Dennis Nkangi <dennis.nkangi@odevo.com>
2026-06-01 10:44:30 +0200

    Added post, patch and delete routes

--- backend/routes/songRoutes.js
+++ backend/routes/songRoutes.js
@@ -9,4 +9,39 @@ router.get("/", (req, res) => {
     .catch(err => res.status(500).json({ error: err.message }))
 })
 
+router.post("/", (req, res) => {
+  const { title, artist, spotifyUrl } = req.body
+  const newSong = new Song({ title, artist, spotifyUrl })
+
+  newSong.save()
+    .then(song => res.status(201).json(song))
+    .catch(err => res.status(500).json({ error: err.message }))
+})
+
+router.patch("/:id/banger", (req, res) => {
+  const { id } = req.params
+  const { title, artist, spotifyUrl } = req.body 
+
+  Song.findByIdAndUpdate(id, { $inc: { bangerVotes: 1 } }, { new: true })
+    .then(song => res.json(song))
+    .catch(err => res.status(500).json({ error: err.message }))
+})
+
+router.patch("/:id/cringe", (req, res) => {
+  const { id } = req.params
+  const { title, artist, spotifyUrl } = req.body 
+
+  Song.findByIdAndUpdate(id, { $inc: { cringeVotes: 1 } }, { new: true })
+    .then(song => res.json(song))
+    .catch(err => res.status(500).json({ error: err.message }))
+})
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params
+
+  Song.findByIdAndDelete(id)
+    .then(() => res.json({ message: "Song deleted" }))
+    .catch(err => res.status(500).json({ error: err.message }))
+})
+
 export default router
